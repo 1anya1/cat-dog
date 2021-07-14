7e3067da4a8dea550f1a3966cc74fff81d8f0440
Anna Filatova <filatovaa@yahoo.com>
2021-07-13 22:01:22 -0700

    working on layout for mobile, web and tablet views

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ function mouseOut(event){
 function determineWinner(winningRow){
     for(i=0; i<winningRow.length; i++){
         // document.getElementById(winningRow[i]).style.backgroundBlendMode='luminosity';
-        document.getElementById(winningRow[i]).style.boxShadow='inset 0 0 0 2000px rgba(26,165,221, 0.6)';
+        document.getElementById(winningRow[i]).style.boxShadow='inset 0 0 0 2000px rgba(225,225,225, 0.6)';
         
     }
     if(player ===1){
